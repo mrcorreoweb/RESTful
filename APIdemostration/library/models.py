1b9f714bc97f19bc6e64532b8c52a6c00a5c9fe7
mrcorreoweb <145986471+mrcorreoweb@users.noreply.github.com>
2024-08-14 18:19:32 +0000

    setup finished, models done, migrations made

--- APIdemostration/library/models.py
+++ APIdemostration/library/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create classes for two entities: 📚 **Books** and ✍️ **Writers**
+
+
+class Writer(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_date = models.DateField()
+    writer = models.ForeignKey(Writer, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return str(self.title)
