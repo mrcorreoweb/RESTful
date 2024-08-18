681b8052df121af34c2fb82d20d807eab0dcc639
mrcorreoweb <145986471+mrcorreoweb@users.noreply.github.com>
2024-08-18 22:06:08 +0000

    first working version with only listingviews

--- APIdemostration/library/models.py
+++ APIdemostration/library/models.py
@@ -1,19 +1,21 @@
-from django.db import models
+""" Create classes for two entities: 📚 **Books** and ✍️ **Writers**"""
 
-# Create classes for two entities: 📚 **Books** and ✍️ **Writers**
+from django.db import models
 
 
 class Writer(models.Model):
+    """Represents a writer in the library."""
+
     name = models.CharField(max_length=100)
-    birth_date = models.DateField()
 
     def __str__(self):
         return str(self.name)
 
 
 class Book(models.Model):
+    """Represents a book in the library."""
+
     title = models.CharField(max_length=200)
-    publication_date = models.DateField()
     writer = models.ForeignKey(Writer, on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
