c33a88418d1f6206ce2b0b9ce907cf5fbba52763
mrcorreoweb <145986471+mrcorreoweb@users.noreply.github.com>
2024-08-18 22:06:37 +0000

    first working version

---
+++ APIdemostration/library/urls.py
@@ -0,0 +1,9 @@
+"""Set up the url routes for the library application."""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
+    path('authors/', views.author_list, name='author_list'),
+]
