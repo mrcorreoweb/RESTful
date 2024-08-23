1332585a4f937433d9b3a56d52d5952967c5ac4d
mrcorreoweb <145986471+mrcorreoweb@users.noreply.github.com>
2024-08-23 11:36:27 +0000

    CRUD operations tested in FBVs and working

--- API/library/urls.py
+++ API/library/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
-    path('writers/', views.writer_list, name='writer_list'),
+    path("writers/", views.writer_list, name="writer_list"),
+    path("writers/<int:pk>/", views.writer_detail, name="writer_detail"),
+    path("books/", views.book_list, name="book_list"),
+    path("books/<int:pk>/", views.book_detail, name="book_detail"),
 ]
--- API/library/views.py
+++ API/library/views.py
@@ -1,32 +1,157 @@
-"""Implement the views to return JSON responses for the following API endpoints:
+"""Implement the views to return JSON responses and perform CRUD operations
+for the following API endpoints (method, URL, description):
+For writers:
+- GET, /writers, return a list of all writers in the library
+- POST, /writers, create a new writer in the library
+- GET, /writers/<writer_id>, return the details of a specific writer
+- PUT, /writers/<writer_id>, update the details of a specific writer
+- DELETE, /writers/<writer_id>, delete a specific writer from the library
+For books:
+- GET, /books, return a list of all books in the library
+- POST, /books, create a new book in the library.
+- GET, /books/<book_id>, return the details of a specific book
+- PUT, /books/<book_id>, update the details of a specific book.
+- DELETE, /books/<book_id>, delete a specific book from the library.
 
-1. /books: Retrieve a list of all books in the library.
-2. /writers: Retrieve a list of all authors in the library."""
+Method of implementation: 
+- Function Based Views (FBVs)
+"""
 
-from django.http import JsonResponse
+import json
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.decorators.http import require_http_methods
 from .models import Book, Writer
 
 
-def book_list(request): #pylint:disable=unused-argument
-    """Return a list of all books in the library."""
+# Writers Endpoints
+@require_http_methods(["GET", "POST"])
+@csrf_exempt
+def writer_list(request):  # pylint:disable=unused-argument
+    """
+    GET, /writers, return a list of all writers in the library.
+    POST, /writers, create a new writer in the library.
+    """
+    if request.method == "GET":
+        writers = Writer.objects.all()  # pylint:disable=no-member
+        writer_data = [{"name": writer.name} for writer in writers]
+        return JsonResponse(writer_data, safe=False)
+    elif request.method == "POST":
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return HttpResponse(status=400)
+        try:
+            name = data.get("name")
+            writer = Writer(name=name)
+            writer.save()
+            writer_data = {"name": writer.name}
+            return JsonResponse(writer_data, status=201)
+        except KeyError:
+            return HttpResponse(status=400)
 
-    books = Book.objects.all() #pylint:disable=no-member
-    book_data = [
-        {
-            "title": book.title,            
-            "writer": book.writer.name,
-        }
-        for book in books
-    ]
-    return JsonResponse(book_data, safe=False)
+@csrf_exempt
+@require_http_methods(["GET", "PUT", "DELETE"])
+def writer_detail(request, writer_id):  # pylint:disable=unused-argument
+    """
+    GET, /writers/<writer_id>, return the details of a specific writer
+    PUT, /writers/<writer_id>, update the details of a specific writer
+    DELETE, /writers/<writer_id>, delete a specific writer from the library
+    """
+    try:
+        writer = Writer.objects.get(pk=writer_id)  # pylint:disable=no-member
+    except ObjectDoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        writer_data = {"name": writer.name}
+        return JsonResponse(writer_data)
+
+    elif request.method == "PUT":
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return HttpResponse(status=400)
+        try:
+            name = data.get("name")
+            writer.name = name
+            writer.save()
+            writer_data = {"name": writer.name}
+            return JsonResponse(writer_data)
+        except KeyError:
+            return HttpResponse(status=400)
+
+    elif request.method == "DELETE":
+        writer.delete()
+        return HttpResponse(status=204)
 
 
-def writer_list(request): #pylint:disable=unused-argument
-    """Return a list of all writers in the library."""
+# Books Endpoints
+@require_http_methods(["GET", "POST"])
+@csrf_exempt
+def book_list(request):  # pylint:disable=unused-argument
+    """
+    GET, /books, return a list of all books in the library.
+    POST, /books, create a new book in the library.
+    """
+    if request.method == "GET":
+        books = Book.objects.all()  # pylint:disable=no-member
+        book_data = [
+            {"title": book.title, "writer": book.writer.name} for book in books
+        ]
+        return JsonResponse(book_data, safe=False)
+    elif request.method == "POST":
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return HttpResponse(status=400)
+        try:
+            title = data.get("title")
+            writer_id = data.get("writer_id")
+            writer = Writer.objects.get(pk=writer_id)  # pylint:disable=no-member
+            book = Book(title=title, writer=writer)
+            book.save()
+            book_data = {"title": book.title, "writer": book.writer.name}
+            return JsonResponse
+        except KeyError:
+            return HttpResponse(status=400)
 
-    writers = Writer.objects.all() #pylint:disable=no-member
-    writer_data = [
-        {"name": writer.name}
-        for writer in writers
-    ]
-    return JsonResponse(writer_data, safe=False)
+
+@csrf_exempt
+@require_http_methods(["GET", "PUT", "DELETE"])
+def book_detail(request, book_id):  # pylint:disable=unused-argument
+    """
+    GET, /books/<book_id>, return the details of a specific book
+    PUT, /books/<book_id>, update the details of a specific book
+    DELETE, /books/<book_id>, delete a specific book from the library
+    """
+    try:
+        book = Book.objects.get(pk=book_id)  # pylint:disable=no-member
+    except ObjectDoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        book_data = {"title": book.title, "writer": book.writer.name}
+        return JsonResponse(book_data)
+
+    elif request.method == "PUT":
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return HttpResponse(status=400)
+        try:
+            title = data.get("title")
+            writer_id = data.get("writer_id")
+            writer = Writer.objects.get(pk=writer_id)  # pylint:disable=no-member
+            book.title = title
+            book.writer = writer
+            book.save()
+            book_data = {"title": book.title, "writer": book.writer.name}
+            return JsonResponse(book_data)
+        except KeyError:
+            return HttpResponse(status=400)
+
+    elif request.method == "DELETE":
+        book.delete()
+        return HttpResponse(status=204)
